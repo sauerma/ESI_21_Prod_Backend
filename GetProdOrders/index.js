750218ac5fd8750e2532e28240953e5395965900
sauerma <msauer1stud.hs-offenburg.de>
2021-05-10 18:30:17 +0200

    Add Lambda Function GetProdOrders

---
+++ GetProdOrders/index.js
@@ -0,0 +1,81 @@
+//-------------------------IMPORTS-------------------------//
+
+const mysql = require('mysql2/promise');
+var config = require('./config');
+
+
+//-------------------------Global variables-------------------------//
+
+var res;
+var results = [];
+
+//-------------------------Database Connection-------------------------//
+const con = {
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  port: config.port
+};
+
+//-------------------------Handler-------------------------//
+
+exports.handler = async (event, context, callback) => {
+  const pool = await mysql.createPool(con);
+
+  try {
+
+    //get all Customers
+    await callDB(pool, getSortedOrders());
+    results = res;
+    console.log(results);
+
+    const response = {
+      statusCode: 200,
+      body: results
+    };
+
+    console.log(response);
+    return response;
+  }
+  catch (error) {
+    console.log(error);
+    return {
+      statusCode: 400,
+      "Error": "Function catched an error"
+    };
+  }
+  finally {
+    await pool.end();
+  }
+};
+
+//-----------------------Helper----------------------//
+
+async function callDB(client, queryMessage) {
+
+  var queryResult;
+  await client.query(queryMessage)
+    .then(
+      (results) => {
+        queryResult = results[0];
+        return queryResult;
+      })
+    .then(
+      (results) => {
+        //queryResult = results[0];
+        console.log(JSON.parse(JSON.stringify(results)));
+        res = JSON.parse(JSON.stringify(results));
+        //console.log(res);
+        return results
+      })
+    .catch(console.log)
+};
+
+//-----------------------Functions----------------------//	
+
+const getSortedOrders = function () {
+  var queryMessage = "SELECT * from production.PLANNING_ORDERS where prod_status = 1 or prod_status = 2 or prod_status = 3;";
+ 
+  return (queryMessage);
+};
+	
