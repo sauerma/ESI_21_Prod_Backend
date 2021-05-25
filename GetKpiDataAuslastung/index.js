e8a240dca28dd8afcf1b2691016cb8ab82fb799d
sauerma <msauer1stud.hs-offenburg.de>
2021-05-25 16:51:42 +0200

    Lambda GetKpiDataAuslastung

---
+++ GetKpiDataAuslastung/index.js
@@ -0,0 +1,82 @@
+//-------------------------IMPORTS-------------------------//
+
+const mysql = require('mysql2/promise');
+var config = require('./config');
+
+
+//-------------------------Global variables-------------------------//
+
+var res;
+var results = [];
+
+//-------------------------Database Connection-------------------------//
+const con = {
+  host: config.host,
+  user: config.user,
+  password: config.password,
+  port: config.port
+};
+
+//-------------------------Handler-------------------------//
+
+exports.handler = async (event, context, callback) => {
+  const pool = await mysql.createPool(con);
+
+  try {
+
+    //get all Customers
+    await callDB(pool, getAuslastung());
+    results = res;
+
+    console.log("Data:", results);
+
+    const response = {
+      statusCode: 200,
+      body: results
+    };
+
+    console.log(response);
+    return response;
+  }
+  catch (error) {
+    console.log(error);
+    return {
+      statusCode: 400,
+      "Error": "Function catched an error"
+    };
+  }
+  finally {
+    await pool.end();
+  }
+};
+
+//-----------------------Helper----------------------//
+
+async function callDB(client, queryMessage) {
+
+  var queryResult;
+  await client.query(queryMessage)
+    .then(
+      (results) => {
+        queryResult = results[0];
+        return queryResult;
+      })
+    .then(
+      (results) => {
+        //queryResult = results[0];
+        console.log(JSON.parse(JSON.stringify(results)));
+        res = JSON.parse(JSON.stringify(results));
+        //console.log(res);
+        return results
+      })
+    .catch(console.log)
+};
+
+//-----------------------Functions----------------------//	
+
+const getAuslastung= function () {
+
+  var queryMessage = "SELECT Round(((SUM(Quantity) / 350) * 100), 2) AS auslastung FROM production.PLANNING_ORDERS WHERE prod_status = 1 ORDER BY END_DATE;";
+
+  return (queryMessage);
+};
