d0db6fa2fa529e23482144f0832d1fe9c1c5a8bf
sauerma <msauer1stud.hs-offenburg.de>
2021-05-28 16:10:46 +0200

    Lambda GetInBestellungOrders

---
+++ GetInBestellungOrders/config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.host = 'production.crosh4an9ahm.eu-central-1.rds.amazonaws.com';
+config.user = 'production';
+config.password = 'Esi-sose21';
+config.port = '3306';
+config.database = 'production'
+
+module.exports = config;
