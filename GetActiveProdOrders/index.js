6078f7d9bda028d6f1a5ab9e23f4ca9faec9b57e
sauerma <msauer1stud.hs-offenburg.de>
2021-05-29 12:59:44 +0200

    Update GetActiveProdOrders to Status "Produziert"

--- GetActiveProdOrders/index.js
+++ GetActiveProdOrders/index.js
@@ -25,7 +25,7 @@ exports.handler = async (event, context, callback) => {
   try {
 
     //get all Customers
-    await callDB(pool, getSortedOrders());
+    await callDB(pool, getActiveProdOrders());
     results = res;
     console.log(results);
 
@@ -73,8 +73,8 @@ async function callDB(client, queryMessage) {
 
 //-----------------------Functions----------------------//	
 
-const getSortedOrders = function () {
-  var queryMessage = "SELECT po1.O_NR, po1.OI_NR, po1.PO_CODE, po1.PO_COUNTER, (select date_format(O_date, '%m.%d.%Y %H:%i') from production.PLANNING_ORDERS po2 where po2.o_nr = po1.o_nr and po2.oi_nr = po1.oi_nr and po2.po_code = po1.po_code and po2.po_counter = po1.po_counter) AS O_DATE, REPLACE (REPLACE(po1.CUSTOMER_TYPE, 'P', 'Privatkunde'), 'B', 'Businesskunde') as CUSTOMER_TYPE, po1.QUANTITY, REPLACE(REPLACE(REPLACE(REPLACE(po1.PROD_STATUS, '0', 'In Planung'), '1', 'In Produktion'), '2', 'Produziert'), '3', 'Ausgelagert') as PROD_STATUS, po1.MAT_NR, po1.C, po1.M, po1.Y, po1.K, po1.HEXCOLOR, po1.PROD_PRIO, po1.IMAGE, DELTA_E, (select date_format(END_DATE, '%m.%d.%Y %H:%i') from production.PLANNING_ORDERS po3 where po3.o_nr = po1.o_nr and po3.oi_nr = po1.oi_nr and po3.po_code = po1.po_code and po3.po_counter = po1.po_counter) AS END_DATE, po1.p_nr FROM production.PLANNING_ORDERS po1 where prod_status = 1 order by prod_prio;";
+const getActiveProdOrders = function () {
+  var queryMessage = "SELECT po1.O_NR, po1.OI_NR, po1.PO_CODE, po1.PO_COUNTER, (SELECT DATE_FORMAT(O_date, '%m.%d.%Y %H:%i') FROM production.PLANNING_ORDERS po2 WHERE po2.o_nr = po1.o_nr AND po2.oi_nr = po1.oi_nr AND po2.po_code = po1.po_code AND po2.po_counter = po1.po_counter) AS O_DATE, REPLACE(REPLACE(po1.CUSTOMER_TYPE, 'P', 'Privatkunde'), 'B', 'Businesskunde') AS CUSTOMER_TYPE, po1.QUANTITY, REPLACE(REPLACE(REPLACE(REPLACE(po1.PROD_STATUS, '0', 'In Planung'), '1', 'In Färbung'), '2', 'In Druck'), '3', 'Produziert') AS PROD_STATUS, po1.MAT_NR, po1.C, po1.M, po1.Y, po1.K, po1.HEXCOLOR, po1.PROD_PRIO, po1.IMAGE, DELTA_E, (SELECT DATE_FORMAT(END_DATE, '%m.%d.%Y %H:%i') FROM production.PLANNING_ORDERS po3 WHERE po3.o_nr = po1.o_nr AND po3.oi_nr = po1.oi_nr AND po3.po_code = po1.po_code AND po3.po_counter = po1.po_counter) AS END_DATE, po1.p_nr FROM production.PLANNING_ORDERS po1 WHERE prod_status = 1 or prod_status = 2 ORDER BY prod_prio;";
   return (queryMessage);
 };
 	
